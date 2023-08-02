b717d017e832612152b2fef1550fff8b7d08bcb4
Rythem Rythem <134351272+RythemRythem@users.noreply.github.com>
2023-08-01 23:33:31 -0400

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,8 @@
+<footer class="my_footer">
+    <div class=" mb-3">
+
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+    <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+</div>
+    </footer>
